ab4806a577c7ed405c9a606e77b6b04faf131c84
Julian Rüth <julian.rueth@fsfe.org>
2025-09-14 18:36:09 +0300

    Drop non-portable globstar from build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -ex
-shopt -s globstar
 
 ./configure --prefix="$PREFIX" || (cat config.log; false)
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-  make check || (cat ./**/test-suite.log; exit 1)
+  make check
 fi
 make install
