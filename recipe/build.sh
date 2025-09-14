eb162e1d4ec4b09ab42c84a84f8dd310fac233a3
Julian Rüth <julian.rueth@fsfe.org>
2025-09-14 19:20:39 +0300

    Enable libtool patch on Windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,8 @@ set -ex
 
 ./configure --prefix="$PREFIX" || (cat config.log; false)
 
+[[ "$target_platform" == "win-64" ]] && patch_libtool
+
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
   make check
