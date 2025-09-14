5f82fa2b8347559e69e25628fd8dadabc3ff69ee
Julian Rüth <julian.rueth@fsfe.org>
2025-09-14 18:01:07 +0300

    Show failing test output in CI

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -ex
+shopt -s globstar
 
 ./configure --prefix="$PREFIX" || (cat config.log; false)
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+  make check || (cat ./**/test-suite.log; exit 1)
 fi
 make install
